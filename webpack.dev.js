5c5ae42a4e87a44d5e09b6e368dcdb9e6117107e
Alejandro Sanchez <aalejo@gmail.com>
2020-02-10 17:01:20 +0000

    updated dotenv

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
-const Dotenv = require('dotenv-webpack');
 
 module.exports = merge(common, {
   devtool: "source-map",
